eea2bcad8023ec48d7c38495d729387844ddefa8
Edilson36 <edilson360ace@gmail.com>
2022-01-26 23:55:59 +0200

    structure project

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Ciber-Coffee</title>
 </head>
 <body>
-    
+    <div class="header">
+        <nav>
+            <div class="brand">
+                <h2><a href="/">Ciber<span>Coffee</span></a></h2>
+            </div>
+            <ul class="navigator">
+                <li class="nav-list"><a href="/">Home</a></li>
+                <li class="nav-list"><a href="#">Network Packet</a></li>
+                <li class="nav-list"><a href="#">Coffee Services</a></li>
+                <li class="nav-list"><a href="views/form-login.php">Sign in</a></li>
+                <li class="nav-list">Sign up</li>
+            </ul>
+        </nav>        
+    </div>
 </body>
 </html>
---
+++ views/form-login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <div class="form-group">
+        <form action="">
+            <label for="email">Username:</label>
+            <input type="email" name="email" id="">
+            <label for="password">Password:</label>
+            <input type="password" name="" id="">
+            <input type="submit" value="Submit">
+            <br>
+            <p>Create a new account? <a href="#">Sign up</a></p>
+        </form>
+    </div>
+</body>
+</html>
